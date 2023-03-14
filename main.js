ae72e9a201f2648e28b66645b093bf18272425ed
Mary83-ops <nabwiremaryann7@gmail.com>
2023-03-14 09:26:39 +0300

    tic tac toe

---
+++ main.js
@@ -0,0 +1,35 @@
+var playerBoard;
+const humanPlayer = 'O';
+const aiPlayer = 'X';
+const winAll = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+    document.querySelector(".endGame").getElementsByClassName.display = "none";
+    playerBoard = Array.from(Array(9).keys());
+    for (var i = 0; i , cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false);
+    }
+}
+
+function turnClick(square) {
+    turnClick(square.target.id, humanPlayer)
+}
+
+function turn(squareId, player) {
+    playerBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player
+}
