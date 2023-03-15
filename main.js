e0dd85fc8cafd6946808a97fe051253c76fe04ae
Mary83-ops <nabwiremaryann7@gmail.com>
2023-03-15 10:44:31 +0300

    tic tac toe

--- main.js
+++ main.js
@@ -1,35 +1,65 @@
-var playerBoard;
-const humanPlayer = 'O';
-const aiPlayer = 'X';
-const winAll = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+function myfunc() {
+  var b1, b2, b3, b4, b5, b6, b7, b8, b9;
+  b1 = document.getElementById("b1").value;
+  b2 = document.getElementById("b2").value;
+  b3 = document.getElementById("b3").value;
+  b4 = document.getElementById("b4").value;
+  b5 = document.getElementById("b5").value;
+  b6 = document.getElementById("b6").value;
+  b7 = document.getElementById("b7").value;
+  b8 = document.getElementById("b8").value;
+  b9 = document.getElementById("b9").value;  
 
-const cells = document.querySelectorAll('.cell');
-startGame();
+  if ((b1 == 'x' || b1 == 'X') && (b2 == 'x' ||
+       b2 == 'X') && (b3 == 'x' || b3 == 'X')) {
+          document.getElementById('message')
+            .innerHTML = "Player X won";
+        document.getElementById("b4").disabled = true;
+        document.getElementById("b5").disabled = true;
+        document.getElementById("b6").disabled = true;
+        document.getElementById("b7").disabled = true;
+        document.getElementById("b8").disabled = true;
+        document.getElementById("b9").disabled = true;
+        window.alert('Player X won');
+        }
 
-function startGame() {
-    document.querySelector(".endGame").getElementsByClassName.display = "none";
-    playerBoard = Array.from(Array(9).keys());
-    for (var i = 0; i , cells.length; i++) {
-        cells[i].innerText = '';
-        cells[i].style.removeProperty('background-color');
-        cells[i].addEventListener('click', turnClick, false);
+  else if ((b1 == 'x' || b1 == 'X') && (b4 == 'x' ||
+        b4 == 'X') && (b7 == 'x' || b7 == 'X')) {
+        document.getElementById('message')
+            .innerHTML = "Player X won";
+        document.getElementById("b2").disabled = true;
+        document.getElementById("b3").disabled = true;
+        document.getElementById("b5").disabled = true;
+        document.getElementById("b6").disabled = true;
+        document.getElementById("b8").disabled = true;
+        document.getElementById("b9").disabled = true;
+ 
+        window.alert('Player X won');
+        }
+
+  else if ((b7 == 'x' || b7 == 'X') && (b8 == 'x' ||
+        b8 == 'X') && (b9 == 'x' || b9 == 'X')) {
+          document.getElementById('message')
+            .innerHTML = "Player X won";
+        document.getElementById("b1").disabled = true;
+        document.getElementById("b2").disabled = true;
+        document.getElementById("b3").disabled = true;
+        document.getElementById("b4").disabled = true;
+        document.getElementById("b5").disabled = true;
+        document.getElementById("b6").disabled = true;
+        window.alert('Player X won');
+        }
+
+        else if ((b3 == 'x' || b3 == 'X') && (b6 == 'x' ||
+        b6 == 'X') && (b9 == 'x' || b9 == 'X')) {
+        document.getElementById('message')
+            .innerHTML = "Player X won";
+        document.getElementById("b1").disabled = true;
+        document.getElementById("b2").disabled = true;
+        document.getElementById("b4").disabled = true;
+        document.getElementById("b5").disabled = true;
+        document.getElementById("b7").disabled = true;
+        document.getElementById("b8").disabled = true;
+        window.alert('Player X won');
     }
-}
-
-function turnClick(square) {
-    turnClick(square.target.id, humanPlayer)
-}
-
-function turn(squareId, player) {
-    playerBoard[squareId] = player;
-    document.getElementById(squareId).innerText = player
 }
