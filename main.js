1ef7d5bdd48d65a0784ba64f7b3c5611dd957596
Mary83-ops <nabwiremaryann7@gmail.com>
2023-03-20 18:14:16 +0300

    tic tac toe

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const WINNING_COMBINATIONS = [
     [0, 4, 8],
     [2, 4, 6]
 ]
-const cellElements = document.querySelectorAll('[data-cell')
+const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 const winningMessageElement = document.getElementById('winningMessage')
 const restartButton = document.getElementById('restartButton')
